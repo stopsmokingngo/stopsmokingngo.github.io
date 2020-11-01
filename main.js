1735937fe2ff8ec41b4b710ca5f553ae9ec55b77
stopsmokingngo <73438595+stopsmokingngo@users.noreply.github.com>
2020-11-01 17:43:27 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,4 +11,14 @@ $(function() {
       $('#cigbox').css('animation', 'unset');
       $('.cig').css('animation', 'unset');
     });
-  });
+});
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
